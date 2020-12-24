698d4eeb316067b7b5c74df9c2472844f206ded8
rgoodman22 <robertgoodman2022@u.northwestern.edu>
2020-12-24 15:00:03 -0500

    status complete

--- TicTacToe-app/src/components/Board/Board.js
+++ TicTacToe-app/src/components/Board/Board.js
@@ -2,20 +2,47 @@ import React, { useState } from 'react';
 import Square from '../Square';
 import './Board.css';
 
-const Board = () => {
-    const [board, setBoard] = useState(Array(9).fill(1))
-    const [turn, setTurn] = useState('X');
-    const [winner, setWinner] = useState('Game is unfished');
-    const status = "Next Player: " + turn;
+const calculateStatus = (board) => {
+    const possibleLines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    const winner = possibleLines.find(line => board[line[0]] !== '' && 
+                                                       board[line[0]] === board[line[1]] && 
+                                                       board[line[0]] === board[line[2]]);
+    const isFull = board.find(square => square === '');
+    if (winner === undefined) {
+        if (isFull !== undefined) {
+            return 'Game is incomplete';
+        } else {
+            return 'Game is a draw';
+        }
+    } else {
+        return 'Winner: ' + board[winner[0]];
+    }
+}
 
-    const renderSquare = (i) => (
-        <Square pos = {i} boardState = {{board, setBoard}} turnState = { {turn, setTurn }} />
-    );
+
+
+const Board = () => {
+    const [board, setBoard] = useState(Array(9).fill(''))
+    const [turn, setTurn] = useState('X');
+    const status = calculateStatus(board);
+
+    const renderSquare = (i) => {
+        return (
+        <Square pos = {i} boardState = {{board, setBoard}} turnState = { {turn, setTurn }} status = {status}/>  
+    )};
     
     return (
         <div>
             <div className="status">{status}</div>
-            <div className="winner">{winner}</div>
             <div className="board-row">
                 {renderSquare(0)}
                 {renderSquare(1)}
--- TicTacToe-app/src/components/Square/Square.js
+++ TicTacToe-app/src/components/Square/Square.js
@@ -2,19 +2,21 @@ import React, {useState} from 'react';
 import './Square.css';
 
 
-const Square = ({pos, boardState, turnState}) => {
+const Square = ({pos, boardState, turnState, status}) => {
     const [player, setPlayer] = useState('');
     
     return (
     <button className="square"
             onClick = { () => {
-                    if (player === '') {
+                if (player !== '' || status === "Game is a draw" || status.substring(0,6) === "Winner") {
+                    return
+                } else{
                         const newBoard = boardState.board.slice();
                         newBoard[pos] = turnState.turn;
                         boardState.setBoard(newBoard);
                         if (turnState.turn === 'X' ? turnState.setTurn('O') : turnState.setTurn('X'));
                         setPlayer(turnState.turn);
-                    } 
+                    }
                 }
             }>
         { player }
