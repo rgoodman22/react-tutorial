4a2a078e3b1d78537f0442eb32d8b84cfe40d605
rgoodman22 <robertgoodman2022@u.northwestern.edu>
2021-01-04 23:35:09 -0500

    img centered

--- shoppingCart-app/src/components/ProductTile.js
+++ shoppingCart-app/src/components/ProductTile.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
@@ -9,20 +9,28 @@ const useStyles = makeStyles((theme) => ({
       width: 200,
     },
     image: {
-        width: 100,
+        width: 175,
         height: "auto"
     }
   }));
 
 const ProductTile = ({product}) => {
+    const [pSize, setSize] = useState(null);
     const classes = useStyles();
     const imgSource = "products/" + product.sku + "_1.jpg";
     console.log(imgSource);
 
     return (
-        <Grid item key={product.sku} justify={"center"}>
+        <Grid item key={product.sku}>
             <Paper className={classes.paper}>
-                <img className = {classes.image} src = {imgSource} alt = {product.description}/>
+                <Grid container spacing={2}
+                                justify="center"
+                                direction="column"
+                                alignItems="center">
+                    <Grid item>
+                        <img className = {classes.image} src = {imgSource} alt = {product.description}/>
+                    </Grid>
+                </Grid>
             </Paper>
         </Grid>
     );
